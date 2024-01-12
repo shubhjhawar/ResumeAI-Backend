5c5ed1d1fc5e9c7174955ac4243297ec73898f89
Shubh <shubhjhawar78@gmail.com>
2024-01-12 18:42:38 -0500

    made changes to predict 3 jobs

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the model and the label encoder
 model = pickle.load(open('model.pkl', 'rb'))
@@ -12,9 +14,14 @@ role_encoder = pickle.load(open('role_encoder.pkl', 'rb'))
 def predict():
     data = request.get_json()
     features = np.array([data[feature] for feature in sorted(data.keys())]).reshape(1, -1)
-    prediction = model.predict(features)
-    role = role_encoder.inverse_transform(prediction)
-    return jsonify({'prediction': role[0]})
+
+    class_probs = model.predict_proba(features)
+
+    top_three_indices = np.argsort(class_probs[0])[::-1][:3]
+
+    top_three_roles = role_encoder.inverse_transform(top_three_indices)
+
+    return jsonify({'predictions': top_three_roles.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
